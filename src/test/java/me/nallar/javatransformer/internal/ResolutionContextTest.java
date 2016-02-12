aa8233bfa64f6b7d9f9fd8519803cda446e360df
Ross Allan <ross@nallar.me>
2016-02-12 20:54:45 +0000

    Move TypeOf test to TypeTest

--- src/test/java/me/nallar/javatransformer/internal/ResolutionContextTest.java
+++ src/test/java/me/nallar/javatransformer/internal/ResolutionContextTest.java
@@ -1,23 +1,9 @@
 package me.nallar.javatransformer.internal;
 
-import me.nallar.javatransformer.api.Type;
 import org.junit.Assert;
 import org.junit.Test;
 
 public class ResolutionContextTest {
-	@Test
-	public void testClassNameToDescriptor() throws Exception {
-		testClassNameToDescriptor("java.lang.String", "Ljava/lang/String;");
-		testClassNameToDescriptor("nallar.test.Outer", "Lnallar/test/Outer;");
-		testClassNameToDescriptor("nallar.test.Outer.Inner", "Lnallar/test/Outer$Inner;");
-		testClassNameToDescriptor("BadNamingScheme.test.Outer", "LBadNamingScheme/test/Outer;");
-		testClassNameToDescriptor("BadNamingScheme.test.Outer.Inner", "LBadNamingScheme/test/Outer$Inner;");
-	}
-
-	private void testClassNameToDescriptor(String in, String expected) {
-		Assert.assertEquals(expected, Type.of(in).descriptor);
-	}
-
 	@Test
 	public void testExtractReal() throws Exception {
 		Assert.assertEquals("ab", ResolutionContext.extractReal("ab<bc>"));
