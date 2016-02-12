3b90c6d65140440c05fe2f0ddbc47d17a7d8c934
Ross Allan <ross@nallar.me>
2016-02-12 20:39:54 +0000

    JavaTransformerTest is now parameterized, and runs on both source and bytecode.

--- src/test/java/me/nallar/javatransformer/api/JavaTransformerTest.java
+++ src/test/java/me/nallar/javatransformer/api/JavaTransformerTest.java
@@ -5,11 +5,35 @@ import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 import org.omg.CORBA.BooleanHolder;
 
 import java.nio.file.*;
+import java.util.*;
 
+@RunWith(Parameterized.class)
 public class JavaTransformerTest {
+	private final Path input;
+	private static final String[] extensions = new String[] {"java", "class"};
+
+	@Parameterized.Parameters
+	public static Collection<Path> paths() {
+		return Arrays.asList(getClassPath(), getSourcePath());
+	}
+
+	private static Path getClassPath() {
+		return JavaTransformer.pathFromClass(JavaTransformerTest.class);
+	}
+
+	private static Path getSourcePath() {
+		return Paths.get("src/test/java/");
+	}
+
+	public JavaTransformerTest(Path input) {
+		this.input = input;
+	}
+
 	@Rule
 	public TemporaryFolder folder = new TemporaryFolder();
 
@@ -28,16 +52,22 @@ public class JavaTransformerTest {
 		BooleanHolder holder = new BooleanHolder(false);
 
 		transformer.addTransformer(c -> {
-			System.out.println(c.getName());
+			System.out.println("Transforming class: " + c.getName() + " of type " + c.getClass().getSimpleName());
 			if (c.getName().equals(targetClass)) {
 				holder.value = true;
 			}
 		});
 
-		transformer.transform(JavaTransformer.pathFromClass(this.getClass()), output);
+		transformer.transform(input, output);
 
 		Assert.assertTrue("Transformer must process " + targetClass, holder.value);
 
-		Assert.assertTrue(Files.exists(output.resolve("me/nallar/javatransformer/api/JavaTransformerTest.class")));
+		Assert.assertTrue(exists(output.resolve("me/nallar/javatransformer/api/JavaTransformerTest.")));
+	}
+
+	private static boolean exists(Path p) {
+		Path base = p.getParent();
+		String fileName = p.getFileName().toString();
+		return Arrays.asList(extensions).stream().anyMatch(it -> Files.exists(base.resolve(fileName + it)));
 	}
 }
