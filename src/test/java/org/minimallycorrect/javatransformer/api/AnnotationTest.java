5a1b5710c9fc69b850d24c44c361d90438fd6773
Ross Allan <ross@nallar.me>
2017-08-07 21:53:27 +0100

    AnnotationTest now tests annotations with default values and with an unmodifiable map used in the constructor

--- src/test/java/org/minimallycorrect/javatransformer/api/AnnotationTest.java
+++ src/test/java/org/minimallycorrect/javatransformer/api/AnnotationTest.java
@@ -4,6 +4,8 @@ import lombok.val;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.*;
+
 public class AnnotationTest {
 	@Test
 	public void testAnnotationConversion() {
@@ -11,5 +13,22 @@ public class AnnotationTest {
 		Assert.assertEquals(Override.class, override.annotationType());
 		val suppressWarning = Annotation.of(Type.of("java.lang.SuppressWarnings"), new String[] { "unchecked" }).toInstance(SuppressWarnings.class);
 		Assert.assertArrayEquals(new String[] { "unchecked" }, suppressWarning.value());
+	}
+
+	@Test
+	public void testAnnotationConversionWithDefaultValues() {
+		val annotationWithDefault = Annotation.of(Type.of("org.minimallycorrect.javatransformer.api.AnnotationWithDefault"), "changed").toInstance(AnnotationWithDefault.class);
+		Assert.assertEquals(1, annotationWithDefault.index());
+		Assert.assertEquals("changed", annotationWithDefault.value());
+	}
+
+	@Test
+	public void testAnnotationConversionWithDefaultValuesAndUnmodifiableMap() {
+		val map = new HashMap<String, Object>();
+		map.put("value", "changed2");
+		map.put("index", 2);
+		val annotationWithDefault2 = Annotation.of(Type.of("org.minimallycorrect.javatransformer.api.AnnotationWithDefault"), Collections.unmodifiableMap(map)).toInstance(AnnotationWithDefault.class);
+		Assert.assertEquals(2, annotationWithDefault2.index());
+		Assert.assertEquals("changed2", annotationWithDefault2.value());
 	}
 }
