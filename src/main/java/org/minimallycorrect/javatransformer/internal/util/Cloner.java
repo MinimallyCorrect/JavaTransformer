5be5e80900ed3acaee5673d7f2fe9f3f23da7346
Luna <git@nyx.nova.fail>
2017-08-30 13:07:10 +0100

    Cloner now supports deep cloning of methodnodes and cloning of instruction lists

--- src/main/java/org/minimallycorrect/javatransformer/internal/util/Cloner.java
+++ src/main/java/org/minimallycorrect/javatransformer/internal/util/Cloner.java
@@ -2,8 +2,10 @@ package org.minimallycorrect.javatransformer.internal.util;
 
 import lombok.experimental.UtilityClass;
 import lombok.val;
-import org.objectweb.asm.tree.FieldNode;
-import org.objectweb.asm.tree.MethodNode;
+import org.minimallycorrect.javatransformer.api.TransformationException;
+import org.objectweb.asm.tree.*;
+
+import java.util.*;
 
 @UtilityClass
 public class Cloner {
@@ -24,6 +26,8 @@ public class Cloner {
 		node.access = other.access;
 		node.name = other.name;
 		node.attrs = other.attrs;
+		node.maxLocals = other.maxLocals;
+		node.maxStack = other.maxStack;
 		node.annotationDefault = other.annotationDefault;
 		node.exceptions = other.exceptions;
 		node.instructions = other.instructions;
@@ -38,5 +42,81 @@ public class Cloner {
 		node.localVariables = other.localVariables;
 		node.tryCatchBlocks = other.tryCatchBlocks;
 		return node;
+	}
+
+	public MethodNode deepClone(MethodNode other) {
+		val node = new MethodNode();
+		node.desc = other.desc;
+		node.signature = other.signature;
+		node.access = other.access;
+		node.name = other.name;
+		node.attrs = clone(other.attrs);
+		node.maxLocals = other.maxLocals;
+		node.maxStack = other.maxStack;
+		node.annotationDefault = other.annotationDefault;
+		node.exceptions = clone(other.exceptions);
+		node.instructions = other.instructions;
+		node.invisibleAnnotations = clone(other.invisibleAnnotations);
+		node.invisibleLocalVariableAnnotations = clone(other.invisibleLocalVariableAnnotations);
+		node.invisibleParameterAnnotations = clone(other.invisibleParameterAnnotations);
+		node.invisibleTypeAnnotations = clone(other.invisibleTypeAnnotations);
+		node.visibleAnnotations = clone(other.visibleAnnotations);
+		node.visibleLocalVariableAnnotations = clone(other.visibleLocalVariableAnnotations);
+		node.visibleParameterAnnotations = clone(other.visibleParameterAnnotations);
+		node.visibleTypeAnnotations = clone(other.visibleTypeAnnotations);
+		node.localVariables = clone(other.localVariables);
+		node.tryCatchBlocks = clone(other.tryCatchBlocks);
+		return node;
+	}
+
+	private static <T> ArrayList<T> clone(List<T> list) {
+		return list == null ? null : new ArrayList<T>(list);
+	}
+
+	private static <T> ArrayList<T>[] clone(List<T>[] lists) {
+		if (lists == null)
+			return null;
+		val newList = new List<?>[lists.length];
+		for (int i = 0; i < lists.length; i++)
+			newList[i] = clone(lists[i]);
+		return (ArrayList<T>[]) newList;
+	}
+
+	public static InsnList clone(InsnList list) {
+		return clone(list, list.getFirst(), list.getLast());
+	}
+
+	public static InsnList clone(InsnList list, AbstractInsnNode first, AbstractInsnNode last) {
+		val cloned = new InsnList();
+		if (first == null && last == null)
+			return cloned;
+
+		val labels = new HashMap<LabelNode, LabelNode>() {
+			@Override
+			public LabelNode get(Object key) {
+				val result = super.get(key);
+				if (result == null)
+					throw new TransformationException("Label " + key + " referenced by copied instruction in " + list + " is out of bounds to be copied " + first + "->" + last);
+				return result;
+			}
+		};
+
+		AbstractInsnNode insn = first;
+		while (true) {
+			if ((insn instanceof LabelNode))
+				labels.put((LabelNode) insn, new LabelNode());
+			if (insn == last)
+				break;
+			insn = insn.getNext();
+		}
+		insn = first;
+		while (true) {
+			cloned.add(insn.clone(labels));
+			if (insn == last)
+				break;
+			insn = insn.getNext();
+		}
+
+		return cloned;
 	}
 }
