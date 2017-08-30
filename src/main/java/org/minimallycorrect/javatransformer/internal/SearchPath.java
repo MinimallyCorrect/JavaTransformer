37f110fa8f8dca1e63173c8daaf655615d15d3ee
Ross Allan <ross@nallar.me>
2017-08-30 22:42:48 +0100

    SearchPath now finds inner classes

--- src/main/java/org/minimallycorrect/javatransformer/internal/SearchPath.java
+++ src/main/java/org/minimallycorrect/javatransformer/internal/SearchPath.java
@@ -1,15 +1,22 @@
 package org.minimallycorrect.javatransformer.internal;
 
+import com.github.javaparser.JavaParser;
+import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.body.TypeDeclaration;
 import lombok.SneakyThrows;
 import lombok.val;
+import org.jetbrains.annotations.Nullable;
 
+import java.io.*;
 import java.nio.file.*;
+import java.nio.file.attribute.*;
 import java.util.*;
 import java.util.zip.*;
 
+// TODO: make this faster by using dumb regexes instead of JavaParser?
+// probably not worth doing
 public class SearchPath {
-	final List<Path> directories = new ArrayList<>();
-	final Set<String> paths = new HashSet<>();
+	final Map<String, String> classNameToPath = new HashMap<>();
 	final List<Path> inputPaths;
 
 	@SneakyThrows
@@ -17,17 +24,49 @@ public class SearchPath {
 		inputPaths = paths;
 		for (Path path : paths)
 			if (Files.isDirectory(path))
-				directories.add(path);
+				Files.walkFileTree(path, new SimpleFileVisitor<Path>() {
+					@Override
+					public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+						val entryName = path.relativize(file).toString().replace(File.separatorChar, '/');
+						if (entryName.endsWith(".java")) {
+							val parsed = JavaParser.parse(file);
+							findPaths(entryName, parsed);
+						}
+						return super.visitFile(file, attrs);
+					}
+				});
 			else if (Files.isRegularFile(path))
 				try (val zis = new ZipInputStream(Files.newInputStream(path))) {
 					ZipEntry e;
-					while ((e = zis.getNextEntry()) != null) {
-						val name = e.getName().toLowerCase();
-						if (name.endsWith(".class") || name.endsWith(".java"))
-							this.paths.add(e.getName());
-					}
+					while ((e = zis.getNextEntry()) != null)
+						findPaths(e, zis);
 				}
+	}
 
+	private void findPaths(ZipEntry e, ZipInputStream zis) {
+		val entryName = e.getName();
+		if (!e.getName().endsWith(".java"))
+			return;
+		val parsed = JavaParser.parse(zis);
+		findPaths(entryName, parsed);
+	}
+
+	private void findPaths(String path, CompilationUnit compilationUnit) {
+		val typeNames = compilationUnit.getTypes();
+		val packageDeclaration = compilationUnit.getPackageDeclaration().orElse(null);
+		val prefix = packageDeclaration == null ? "" : packageDeclaration.getNameAsString() + '.';
+		for (TypeDeclaration<?> typeDeclaration : typeNames)
+			findPaths(path, typeDeclaration, prefix);
+	}
+
+	private void findPaths(String path, TypeDeclaration<?> typeDeclaration, String packagePrefix) {
+		val parent = typeDeclaration.getParentNode().orElse(null);
+		String name;
+		if (parent instanceof TypeDeclaration)
+			name = packagePrefix + ((TypeDeclaration) parent).getNameAsString() + '.' + typeDeclaration.getNameAsString();
+		else
+			name = packagePrefix + typeDeclaration.getNameAsString();
+		classNameToPath.put(name, path);
 	}
 
 	@Override
@@ -35,13 +74,12 @@ public class SearchPath {
 		return "SearchPath: " + inputPaths;
 	}
 
-	public boolean exists(String s) {
-		String name = s.replace('.', '/') + ".java";
-		if (paths.contains(name))
-			return true;
-		for (Path directory : directories)
-			if (Files.exists(directory.resolve(name)))
-				return true;
-		return false;
+	public boolean hasClass(String className) {
+		return classNameToPath.containsKey(className);
+	}
+
+	@Nullable
+	public String getPathForClass(String className) {
+		return classNameToPath.get(className);
 	}
 }
