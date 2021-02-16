fd82f77ea1f4d5a2471a334f364f0b092d2a3054
Luna <git@nyx.nova.fail>
2021-02-16 21:44:00 +0000

    Reorganise deps, remove unnecessary customisation of plugin repositories

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,15 +26,17 @@ minimallyCorrectDefaults {
 }
 
 dependencies {
-	val lombok = "org.projectlombok:lombok:1.18.18"
-
 	testImplementation("junit:junit:4.13.2")
-	implementation("org.ow2.asm:asm:9.1")
-	implementation("org.ow2.asm:asm-util:9.1")
-	implementation("org.ow2.asm:asm-tree:9.1")
 	implementation("com.github.javaparser:javaparser-core:3.6.24")
 	api("com.google.code.findbugs:jsr305:3.0.2")
 	api("org.jetbrains:annotations:20.1.0")
+
+	val asmVer = "9.1"
+	implementation("org.ow2.asm:asm:$asmVer")
+	implementation("org.ow2.asm:asm-util:$asmVer")
+	implementation("org.ow2.asm:asm-tree:$asmVer")
+
+	val lombok = "org.projectlombok:lombok:1.18.18"
 	implementation(lombok)
 	annotationProcessor(lombok)
 	testAnnotationProcessor(lombok)
